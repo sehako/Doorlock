0b27df224da56debb0e911b5b1880146b197a217
sehako <dhtpgkr1999@gmail.com>
2023-05-07 13:54:51 +0900

    리사이클러뷰 구현 완료

---
+++ app/src/main/java/com/example/doorlock/UserListAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.doorlock
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class UserListAdapter(val context: Context, val userList: ArrayList<Users>) : RecyclerView.Adapter<UserListAdapter.Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder?.bind(userList[position], context)
+    }
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name = itemView.findViewById<TextView>(R.id.tv_name)
+        val date = itemView.findViewById<TextView>(R.id.tv_date)
+        val img = itemView.findViewById<ImageView>(R.id.img_photo)
+
+        fun bind (userList: Users, context: Context) {
+            if (userList.img != "") {
+                val resourceId = context.resources.getIdentifier(userList.img, "drawable", context.packageName)
+                img?.setImageResource(resourceId)
+            } else {
+                img?.setImageResource(R.mipmap.ic_launcher)
+            }
+            name.text = userList.name
+            date.text = userList.date
+        }
+    }
+}
