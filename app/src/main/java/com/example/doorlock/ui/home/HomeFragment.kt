243393aa1785b39c114c1dff82b7da1174f3c542
excodaw <dhtpgkr1999@gmail.com>
2023-05-15 11:30:56 +0900

    사용자 추가 옵션 앱 바에 구현

--- app/src/main/java/com/example/doorlock/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/doorlock/ui/home/HomeFragment.kt
@@ -1,12 +1,18 @@
 package com.example.doorlock.ui.home
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,18 +21,27 @@ import com.example.doorlock.UserListAdapter
 import com.example.doorlock.Users
 
 class HomeFragment : Fragment() {
-    var userList = arrayListOf<Users>(
+
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    private var userList = arrayListOf<Users>(
         Users("소순성", "2023/05/06", ""),
         Users("이종석", "2023/05/06", ""),
         Users("안진원", "2023/05/06", ""),
         Users("오세학", "2023/05/05", "")
     )
 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val users: RecyclerView = view.findViewById(R.id.rv_profile)
 
@@ -36,21 +51,24 @@ class HomeFragment : Fragment() {
         val linearmanager = LinearLayoutManager(requireContext())
         users.layoutManager = linearmanager
 
-        val add: Button = view.findViewById(R.id.camera_test)
-        add.setOnClickListener(View.OnClickListener {
-            val items = arrayOf("사진 촬영", "갤러리")
-            val builder = AlertDialog.Builder(context)
-                .setTitle("사용자 추가").setItems(items) { dialog, which ->
-                    if (which == 0) {
-                        Toast.makeText(context, "카메라 촬영", Toast.LENGTH_LONG).show()
-                    }
-                    else {
-                        Toast.makeText(context, "갤러리 접근", Toast.LENGTH_LONG).show()
-                    }
-                }
-            builder.show()
-            }
-        )
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.user_add, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.add_camera -> {
+
+                true
+            }
+            R.id.add_gallery -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
