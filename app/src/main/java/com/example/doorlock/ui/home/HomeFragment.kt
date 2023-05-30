06b5b764ae7041f0c5a00db09baa678ec278ee52
순성 <sunseongso@github.com>
2023-05-30 22:26:41 +0900

    retrofit

--- app/src/main/java/com/example/doorlock/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/doorlock/ui/home/HomeFragment.kt
@@ -23,6 +23,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.doorlock.R
 import com.example.doorlock.UserListAdapter
 import com.example.doorlock.Users
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 import java.io.File
 
 
@@ -57,21 +71,23 @@ class HomeFragment : Fragment() {
 //        { uri ->
 //            Toast.makeText(requireContext(), "" + uri, Toast.LENGTH_LONG).show()
 //        }
-       val galLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-        { result -> if (result.resultCode == Activity.RESULT_OK)
-            {
-                // Handle the result from the launched activity here
+        val galLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = result.data
-                // Image의 상대경로를 가져온다
                 image = data?.data
-                // 절대경로를 가져오는 함수
-                val imagePath: String? = getPathFromUri(image)
+                val imagePath = getPathFromUri(image)
                 Toast.makeText(requireContext(), "" + imagePath, Toast.LENGTH_LONG).show()
-//                // File변수에 File을 집어넣는다
-                val destFile = imagePath?.let { File(it) }
-                // Process the data
+
+                if (imagePath != null) {
+                    uploadImageToServer(imagePath) // 이미지를 서버로 업로드
+                } else {
+                    Toast.makeText(requireContext(), "Invalid image file.", Toast.LENGTH_LONG).show()
+                }
             }
         }
+
+
+
 
         val menuHost: MenuHost = requireActivity()
         menuHost.addMenuProvider(object : MenuProvider
@@ -128,17 +144,52 @@ class HomeFragment : Fragment() {
     fun getPathFromUri(uri: Uri?): String? {
         uri ?: return null
 
-        val cursor = requireActivity().contentResolver.query(uri, null, null, null, null)
-        cursor?.moveToNext()
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = requireActivity().contentResolver.query(uri, projection, null, null, null)
+        cursor?.use { c ->
+            if (c.moveToFirst()) {
+                val columnIndex = c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                if (columnIndex != -1) {
+                    return c.getString(columnIndex)
+                }
+            }
+        }
+        return null
+    }
 
-        val columnIndex = cursor?.getColumnIndex("_data")
-        val path = if (columnIndex != null && columnIndex >= 0) {
-            cursor.getString(columnIndex)
-        } else {
-            null
+    private fun uploadImageToServer(imagePath: String?) {
+        val file = File(imagePath)
+        val requestFile: RequestBody? = file.asRequestBody("image/*".toMediaTypeOrNull())
+        val body: MultipartBody.Part? = requestFile?.let {
+            MultipartBody.Part.createFormData("image", file.name, it)
         }
 
-        cursor?.close()
-        return path
+        if (body != null) {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://52.79.155.171")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val uploadAPIs = retrofit.create(UploadAPIs::class.java)
+            val call: Call<ResponseBody> = uploadAPIs.uploadImage(body)
+
+            call.enqueue(object : Callback<ResponseBody> {
+                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                    Toast.makeText(requireContext(), "Image uploaded successfully.", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    Toast.makeText(requireContext(), "Image upload failed.", Toast.LENGTH_LONG).show()
+                }
+            })
+        } else {
+            Toast.makeText(requireContext(), "Invalid image file.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    interface UploadAPIs {
+        @Multipart
+        @POST("upload.php")
+        fun uploadImage(@Part image: MultipartBody.Part): Call<ResponseBody>
     }
 }
