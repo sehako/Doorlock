a5bcf07a25243a44cc4e83f7940ff0cf9e95ec71
sehako <dhtpgkr1999@gmail.com>
2023-06-20 11:45:08 +0900

    잠금 해제 버튼 UI 구성

--- app/src/main/java/com/example/doorlock/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/doorlock/ui/dashboard/DashboardFragment.kt
@@ -4,39 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.doorlock.R
 import com.example.doorlock.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
-
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val switch: Switch = view.findViewById(R.id.unlock)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        switch.setOnCheckedChangeListener({_, isChecked ->
+            val message = if (isChecked) "잠금해제" else "잠금"
+            Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+        })
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return view
     }
 }
